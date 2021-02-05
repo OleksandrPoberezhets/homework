5e81ce3500459d9ab7574111aeb90262541f463c
OleksandrPoberezhets <78326641+OleksandrPoberezhets@users.noreply.github.com>
2021-02-05 21:10:57 +0200

    hw3

---
+++ 1.1.2.cpp
@@ -0,0 +1,46 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+int main()
+{
+    double x1,x2,x3;
+    
+    cout<<"Перше число:"<<endl;
+    cin>>x1;
+    cout<<"Друге число:"<<endl;
+    cin>>x2;
+    cout<<"Третє число:"<<endl;
+    cin>>x3;
+    double nums[3]={x1,x2,x3};
+    double max=x1;
+    double min=x2;
+    for(int i =0;i<3;i++){
+        if(abs(nums[i])>abs(max)){
+            max = nums[i];
+        }
+        if(abs(nums[i])<abs(min)){
+            min = nums[i];
+        }
+    }
+    
+    cout<<"a)Найбільше за модулем число:"<<endl;
+    cout<<max<<endl;
+    cout<<"b)Найменше за модулем число:"<<endl;
+    cout<<min<<endl;
+    
+ 
+
+    return 0;
+}
+
